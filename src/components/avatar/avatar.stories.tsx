24c065bae3409f469f4d6c3ef901e822689d0cd9
batarse1 <00014419@uca.edu.sv>
2023-11-25 13:32:48 -0600

    ✏️ Fix typo in avatar.stories.tsx

--- src/components/avatar/avatar.stories.tsx
+++ src/components/avatar/avatar.stories.tsx
@@ -28,7 +28,7 @@ export const Medium: Story = {
   },
 };
 
-export const large: Story = {
+export const Large: Story = {
   args: {
     src: 'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     alt: 'User profile',
