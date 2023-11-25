b97fc005aeb57b8650fb3f69b098f33b763de341
batarse1 <00014419@uca.edu.sv>
2023-11-24 20:45:40 -0600

    ✅ Update tab test to manage md:border-r-4

--- src/components/sidebar/tab/tab.test.tsx
+++ src/components/sidebar/tab/tab.test.tsx
@@ -19,13 +19,14 @@ describe('Tab Component', () => {
   it('renders correctly with given props', () => {
     render(<Tab text="Home" icon={mockIcon} href="/home" />);
     expect(screen.getByText('Home')).toBeInTheDocument();
+    expect(screen.getByRole('link')).toHaveClass('hover:text-primary-4');
     expect(screen.getByRole('link')).toHaveAttribute('href', '/home');
   });
 
   it('applies active class when pathname matches href', () => {
     mockUsePathname.mockReturnValue('/home');
     render(<Tab text="Home" icon={mockIcon} href="/home" />);
-    expect(screen.getByRole('link')).toHaveClass('border-r-4');
+    expect(screen.getByRole('link')).toHaveClass('md:border-r-4');
   });
 
   it('does not apply active class when pathname does not match href', () => {
