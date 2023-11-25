acb97816de3a294ea0ce78fd9fed445cf6a9724a
batarse1 <00014419@uca.edu.sv>
2023-11-25 00:42:43 -0600

    ♻️ Refactor routes of sidebar component

---
+++ src/components/sidebar/routes.tsx
@@ -0,0 +1,5 @@
+import Dashboard from '@/assets/svg/dashboard';
+
+const routes = [{ text: 'dashboard', href: '/', icon: <Dashboard fill="currentColor" /> }];
+
+export default routes;
--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -1,9 +1,9 @@
 import Link from 'next/link';
 
-import Dashboard from '@/assets/svg/dashboard';
 import Ravn from '@/assets/svg/ravn';
 
 import Tab from './tab';
+import routes from './routes';
 
 const Sidebar = () => (
   <div className="flex h-full flex-col rounded-3xl bg-neutral-4 py-3 md:space-y-11">
@@ -11,7 +11,9 @@ const Sidebar = () => (
       <Ravn fill="white" size={40} />
     </Link>
     <div className="flex text-center text-body-s text-neutral-2 md:flex-col md:text-start md:text-body-m">
-      <Tab text="dashboard" href="/" icon={<Dashboard fill="currentColor" />} />
+      {routes.map(({ text, href, icon }) => (
+        <Tab text={text} href={href} icon={icon} />
+      ))}
     </div>
   </div>
 );
