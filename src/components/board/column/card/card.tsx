e3f78dd2e9715b228e3cfd3724e75a74f06d968d
batarse1 <00014419@uca.edu.sv>
2023-11-26 10:49:39 -0600

    ✨ Add convertPointEstimateToPointValue to Card

--- src/components/board/column/card/card.tsx
+++ src/components/board/column/card/card.tsx
@@ -2,42 +2,53 @@ import { RiMoreFill, RiAttachment2, RiNodeTree, RiChat3Line, RiAlarmLine } from 
 
 import Label from '@/components/label';
 import Avatar from '@/components/user/avatar';
+import { TasksQuery } from '@/gql/graphql';
 
+import { convertPointEstimateToPointValue } from '@/utils/formatters';
 import Reaction from './reaction';
 
-const Card = () => (
-  <article className="space-y-4 rounded-lg bg-neutral-4 p-4 text-neutral-1">
-    <div className="flex justify-between space-x-2">
-      <h4 className="text-body-l font-semibold text-neutral-1">Slack</h4>
-      <RiMoreFill size="24px" className="text-neutral-2" />
-    </div>
-    <div className="flex justify-between space-x-2">
-      <p className="text-body-m font-semibold text-neutral-1">4 points</p>
-      <Label
-        className="bg-neutral-2 bg-opacity-10 text-neutral-1"
-        icon={<RiAlarmLine size="24px" />}
-        text="Today"
-      />
-    </div>
-    <ul className="m-[-1rem] flex flex-wrap justify-start space-x-2">
-      <Label className="ml-2 mt-2 bg-secondary-4 bg-opacity-10 text-secondary-4" text="ios app" />
-      <Label className="ml-2 mt-2 bg-tertiary-4 bg-opacity-10 text-tertiary-4" text="android" />
-    </ul>
-    <div className="flex justify-between space-x-2">
-      <Avatar
-        alt="User profile"
-        className="h-8 w-8"
-        src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-      />
-      <ul className="flex items-center space-x-4">
-        <li className="py-1 text-body-m text-neutral-1">
-          <RiAttachment2 size="16px" />
-        </li>
-        <Reaction count={5} icon={<RiNodeTree size="16px" />} />
-        <Reaction count={5} icon={<RiChat3Line size="16px" />} />
+interface CardProps {
+  task: TasksQuery['tasks'][0];
+}
+
+const Card = ({ task }: CardProps) => {
+  const { name, pointEstimate } = task;
+  return (
+    <article className="space-y-4 rounded-lg bg-neutral-4 p-4 text-neutral-1">
+      <div className="flex justify-between space-x-2">
+        <h4 className="text-body-l font-semibold text-neutral-1">{name}</h4>
+        <RiMoreFill size="24px" className="text-neutral-2" />
+      </div>
+      <div className="flex justify-between space-x-2">
+        <p className="text-body-m font-semibold text-neutral-1">
+          {convertPointEstimateToPointValue(pointEstimate)} points
+        </p>
+        <Label
+          className="bg-neutral-2 bg-opacity-10 text-neutral-1"
+          icon={<RiAlarmLine size="24px" />}
+          text="Today"
+        />
+      </div>
+      <ul className="m-[-1rem] flex flex-wrap justify-start space-x-2">
+        <Label className="ml-2 mt-2 bg-secondary-4 bg-opacity-10 text-secondary-4" text="ios app" />
+        <Label className="ml-2 mt-2 bg-tertiary-4 bg-opacity-10 text-tertiary-4" text="android" />
       </ul>
-    </div>
-  </article>
-);
+      <div className="flex justify-between space-x-2">
+        <Avatar
+          alt="User profile"
+          className="h-8 w-8"
+          src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        />
+        <ul className="flex items-center space-x-4">
+          <li className="py-1 text-body-m text-neutral-1">
+            <RiAttachment2 size="16px" />
+          </li>
+          <Reaction count={5} icon={<RiNodeTree size="16px" />} />
+          <Reaction count={5} icon={<RiChat3Line size="16px" />} />
+        </ul>
+      </div>
+    </article>
+  );
+};
 
 export default Card;
--- src/components/board/column/column.tsx
+++ src/components/board/column/column.tsx
@@ -9,9 +9,11 @@ interface ColumnProps {
 
 const Column = ({ text, tasks }: ColumnProps) => (
   <section className="flex h-full w-96 flex-col space-y-4 bg-neutral-5">
-    <h3 className="text-body-l font-semibold capitalize text-neutral-1">{text}</h3>
+    <h3 className="text-body-l font-semibold capitalize text-neutral-1">
+      {text} ({tasks.length})
+    </h3>
     {tasks.map((task) => (
-      <Card key={task.id} />
+      <Card key={task.id} task={task} />
     ))}
   </section>
 );
