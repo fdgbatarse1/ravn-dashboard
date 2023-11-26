0431378e677d028239f776eabbd0473d29c1908e
batarse1 <00014419@uca.edu.sv>
2023-11-25 19:16:58 -0600

    ✨ Add error and not found page

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,7 +1,9 @@
-const NotFound = () => (
+'use client';
+
+const ErrorPage = () => (
   <section className="grid h-full w-full place-items-center text-center text-body-l font-semibold md:text-display-xs">
-    <h1>Page not found</h1>
+    <h1>Oops! Something went wrong.</h1>
   </section>
 );
 
-export default NotFound;
+export default ErrorPage;
