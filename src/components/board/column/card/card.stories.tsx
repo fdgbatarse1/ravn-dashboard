e990a93d781a5b2b44b51e1da4e11f5d1b10eb30
batarse1 <00014419@uca.edu.sv>
2023-11-26 14:26:34 -0600

    📝 Fix card story

--- src/components/board/column/card/card.stories.tsx
+++ src/components/board/column/card/card.stories.tsx
@@ -1,4 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react';
+import TasksQueryMock from '@/data/mocks/TasksQueryMock';
 
 import Card from './card';
 
@@ -12,4 +13,8 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {};
+export const Default: Story = {
+  args: {
+    task: TasksQueryMock.tasks[0],
+  },
+};
