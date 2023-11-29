92314abbe3f7021729ea230eaac2bcacf6be0776
batarse1 <00014419@uca.edu.sv>
2023-11-28 22:21:02 -0600

    💄 Format confirmation test

--- src/components/confirmation/confirmation.test.tsx
+++ src/components/confirmation/confirmation.test.tsx
@@ -2,33 +2,27 @@ import { render, screen, fireEvent } from '@testing-library/react';
 import Confirmation from './confirmation';
 
 describe('Confirmation', () => {
-    const mockOnConfirm = jest.fn();
-    const mockOnCancel = jest.fn();
+  const mockOnConfirm = jest.fn();
+  const mockOnCancel = jest.fn();
 
-    beforeEach(() => {
-        render(
-            <Confirmation
-                text="Are you sure?"
-                onConfirm={mockOnConfirm}
-                onCancel={mockOnCancel}
-            />
-        );
-    });
+  beforeEach(() => {
+    render(<Confirmation text="Are you sure?" onConfirm={mockOnConfirm} onCancel={mockOnCancel} />);
+  });
 
-    it('renders the confirmation text', () => {
-        const confirmationText = screen.getByText('Are you sure?');
-        expect(confirmationText).toBeInTheDocument();
-    });
+  it('renders the confirmation text', () => {
+    const confirmationText = screen.getByText('Are you sure?');
+    expect(confirmationText).toBeInTheDocument();
+  });
 
-    it('calls the onConfirm function when "Yes" button is clicked', () => {
-        const confirmButton = screen.getByText('Yes');
-        fireEvent.click(confirmButton);
-        expect(mockOnConfirm).toHaveBeenCalled();
-    });
+  it('calls the onConfirm function when "Yes" button is clicked', () => {
+    const confirmButton = screen.getByText('Yes');
+    fireEvent.click(confirmButton);
+    expect(mockOnConfirm).toHaveBeenCalled();
+  });
 
-    it('calls the onCancel function when "No" button is clicked', () => {
-        const cancelButton = screen.getByText('No');
-        fireEvent.click(cancelButton);
-        expect(mockOnCancel).toHaveBeenCalled();
-    });
+  it('calls the onCancel function when "No" button is clicked', () => {
+    const cancelButton = screen.getByText('No');
+    fireEvent.click(cancelButton);
+    expect(mockOnCancel).toHaveBeenCalled();
+  });
 });
